549a76d3b693d4a38ddf07f74f5d72f2ff0ad8dc
nrflemens <nrflemens@gmail.com>
2019-03-27 23:15:27 -0400

    Added plotting of multiple data sets on one scatter plot

--- Cameras/Basler.py
+++ Cameras/Basler.py
@@ -32,13 +32,14 @@ class Basler(Camera):
             # Read Pandas dataframe from a csv
             df = pd.read_csv(self.current_file_path, delimiter='\t', lineterminator='\n')
             
-            # Drop unnecessary columns
-            if '\r' in df.columns:
-                df.drop(['\r'], axis = 1, inplace = True)
         except:
             print('FilePathError: invalid file path')
             self.log.append('FilePathError: invalid file path')
         
+        # Drop unnecessary columns
+        if '\r' in df.columns:
+            df.drop(['\r'], axis = 1, inplace = True)
+
         # Rename centroid labels
         df = df.rename(index=str, columns={"Mx All [px]": "Xc", "My All [px]": "Yc"})
         
@@ -49,7 +50,7 @@ class Basler(Camera):
         df.columns = pd.MultiIndex.from_product([[data_name], list(df.columns)])
 
         # Add this data set to the rest of the data
-        self.data = pd.concat([self.data, df], axis = 1)
+        self.data = pd.concat([self.data, df], axis = 1, sort = False)
 
 
     
@@ -72,20 +73,34 @@ class Basler(Camera):
         if not self._Is_Data_Loaded(self.data):
             self.Load_Data()    
         
-        # Grab centroid X and Y coordinates
-        Xc = self.data["Xc"]
-        Yc = self.data["Yc"]
-
-        # Set scatter plot color based on element index
-        color = self.data.index
-           
         # Create new figure
         fig = plt.figure()
         ax = fig.add_axes([0, 0, 1, 1])
+        
 
-        # Generate scatter plot of centroid data
-        s = ax.scatter(x = Xc, y = Yc, c=np.linspace(0,1,len(color)), cmap='Spectral')
-       
+
+        for data_name in self.data.columns.levels[0]:
+            # The current data set
+            data = self.data[data_name]
+
+            # Keeps track of number of data sets plotted
+            data_count = list(self.data.columns.levels[0]).index(data_name)
+
+            # Grab centroid X and Y coordinates
+            Xc = data["Xc"]
+            Yc = data["Yc"]
+
+            # Set scatter plot color based on element index
+            color = data.index
+            
+            cmaps = ["Blues", "Greens", "Reds", "Purples", "Oranges"]
+            # Generate scatter plot of centroid data
+            s = ax.scatter(x = Xc, y = Yc, c=np.linspace(0,1,len(color)), cmap=cmaps[data_count%len(cmaps)])
+            
+            # Create and label colorbar
+            cb = plt.colorbar(s)
+            cb.set_label('Time [s]')
+            
         # Set default labels
         ax.set_title('Beam Centroid')
         ax.set_xlabel('$X_{c}$ $[\mu m]$')
@@ -94,9 +109,4 @@ class Basler(Camera):
         # Set axis below plotted data
         ax.set_axisbelow(True)
 
-        # Create and label colorbar
-        cb = plt.colorbar(s)
-        cb.set_label('Time [s]')
-
         return ax
-        
